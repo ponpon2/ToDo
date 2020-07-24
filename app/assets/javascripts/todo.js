e88dffd114f78b5d078184815a61fa22a6d6790b
ponpon2 <gkqr84@icloud.com>
2020-07-24 14:18:16 +0900

    todo front commit

---
+++ app/assets/javascripts/todo.js
@@ -0,0 +1,24 @@
+const createTodoList = task => {
+  const html = `
+    <li class="list-group-item d-flex justify-content-between align-items-center">
+        <span>${task}</span>
+        <i class="far fa-trash-alt delete"></i>
+    </li>
+    `;
+
+    list.innerHTML += html;
+}
+
+addTask.addEventListener('submit', e => {
+  // デフォルトのイベントを無効
+  e.preventDefault();
+
+  // タスクに入力した値を空白を除外して格納
+  const task = addTask.add.value.trim();
+  if(task.length) {
+      // Todo List の HTML を作成
+      createTodoList(task);
+      // タスクに入力した文字をクリア
+      addTask.reset();
+  }
+});
--- app/controllers/todoes_controller.rb
+++ app/controllers/todoes_controller.rb
@@ -4,4 +4,16 @@ class TodoesController < ApplicationController
   def new
     @todo = Todo.new
   end
+  def create
+    @toto = Todo.create(todo_params)
+    respond_to do |format|
+      format.json
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:list)
+  end
 end
